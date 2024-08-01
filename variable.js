4b8e0cc084dab92854167d5e56c7d2379f9039ae
Prerana Mehta <149857190+prerana2606@users.noreply.github.com>
2024-08-01 13:33:26 +0530

    JS revision

---
+++ variable.js
@@ -0,0 +1,35 @@
+// a=10
+// function variable() {
+//     {
+//         var a = 50;
+//         console.log(a)
+//     }
+//     console.log(a);
+// }
+
+// variable()
+// console.log(a)
+
+
+//  console.log(a)
+//  var a=5 
+//  console.log(a)
+
+
+// a=5
+// function variable(){
+//     console.log(a)
+// }
+// console.log(a)
+// variable()
+
+
+//  a=5
+//  a=10
+//  console.log(a)
+a=5 ;  
+ function variable(){
+    console.log(a)
+}
+console.log(a);
+     variable();
